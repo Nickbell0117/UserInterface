4a958bc5bfa1b679fcf2e8cbcc59b0cdaade3b71
Nickbell0117 <nickbell117@icloud.com>
2023-04-24 09:50:13 -0700

    Finished 5.2
    Finished 5.2 and on step 3 of 5.3

--- UserInterfaceNBellosoP1/Assets/Scripts/GameManager.cs
+++ UserInterfaceNBellosoP1/Assets/Scripts/GameManager.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
 
     private float spawnRate = 1.0f;
+    private int score;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -28,5 +35,10 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
     }
 }
